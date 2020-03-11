0e3d2b525724c9f95516cf62d60316bf5c609a7d
sovaz1997 <sovaz1997@yandex.ru>
2020-03-11 23:54:08 +0300

    feat: portfolio image borders

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 window.onload = function() {
-  const menu = document.querySelector(".site-header__nav");
-  const menuItems = menu.querySelectorAll(".site-header__nav-link");
-  
-  const activeLink = "site-header__nav-link--active";
-
-  menu.addEventListener("click", (e) => {
-    menuItems.forEach((el) => {
-      el.classList.remove(activeLink);
+  function makeActive(targetClass, container, target) {
+    const activeClass = targetClass + '--active';
+    container.forEach((el) => {
+      el.classList.remove(activeClass);
     });
+    
+    if(target.classList.contains(targetClass)) {
+      target.classList.add(activeClass);
+    }
+  }
 
-    //this.alert(e.target);
-    e.target.classList.add(activeLink);
+  const menu = document.querySelector('.site-header__nav');
+  const menuItemClass = "site-header__nav-link";
+  const menuItems = menu.querySelectorAll('.' + menuItemClass);
+  
+  menu.addEventListener('click', (e) => {
+    makeActive(menuItemClass, menuItems, e.target);
+  });
 
+  const portfolio = document.querySelector('.portfolio__works');
+  const portfolioItemClass = "portfolio__work-image";
+  const portfolioImages = portfolio.querySelectorAll('.' + portfolioItemClass);
+
+  portfolio.addEventListener("click", (e) => {
+    makeActive(portfolioItemClass, portfolioImages, e.target);
   });
 }
