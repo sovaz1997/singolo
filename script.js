a6418c9c692f64f5d7170d7c5639c385e70ce6ef
sovaz1997 <sovaz1997@yandex.ru>
2020-03-13 01:06:25 +0300

    feat: slider implementation

--- script.js
+++ script.js
@@ -30,6 +30,40 @@ window.onload = function() {
     }
   }
 
+  const slider = document.querySelector(".slider");
+  const slides = slider.querySelectorAll(".slide");
+
+  function sliderReset() {
+    let index = 0;
+
+    slides.forEach((it, ind) => {
+      if(it.classList.contains("slide--active")) {
+        index = ind;
+        it.classList.remove("slide--active");
+      }
+    });
+
+    return index;
+  }
+
+  function changeSliderBg(slideNumber) {
+    slider.classList.toggle("slider--red", slideNumber === 0);
+    slider.classList.toggle("slider--blue", slideNumber === 1);
+  }
+
+  function nextSlide() {
+    let index = (sliderReset() + 1) % slides.length;
+    slides[index].classList.add("slide--active");
+    changeSliderBg(index);
+  }
+
+  function prevSlide() {
+    let index = sliderReset() - 1;
+    if(index < 0) index = slides.length - 1;
+    slides[index].classList.add("slide--active");
+    changeSliderBg(index);
+  }
+
   const menu = document.querySelector('.site-header__nav');
   const menuItemClass = "site-header__nav-link";
   const menuItems = menu.querySelectorAll('.' + menuItemClass);
@@ -65,4 +99,18 @@ window.onload = function() {
     const portfolioItems = portfolio.querySelectorAll(".portfolio__work-item");
     shuffleImages(portfolioItems);
   });
+
+
+  const sliderPrevButton = document.querySelector(".slider__arrow--left");
+  const sliderNextButton = document.querySelector(".slider__arrow--right");
+
+
+  sliderPrevButton.addEventListener("click", () => {
+    prevSlide();
+  });
+
+  sliderNextButton.addEventListener("click", () => {
+    nextSlide();
+  });
+
 }
